fcadb985278c89ea792038b548fc21aa9dcd13be
Taewon Kong <tkong9@gatech.edu>
2019-08-28 20:18:04 -0400

    Initial Commit

---
+++ Beautiful Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beautiful Calculator
+//
+//  Created by TAEWON KONG on 8/28/19.
+//  Copyright © 2019 TAEWON KONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
